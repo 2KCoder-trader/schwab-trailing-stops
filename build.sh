244173e29271067e81c02c7ec1d727a3883adf9d
2KCoder-trader <kaidenkrenek@gmail.com>
2026-03-21 23:12:59 -0700

    Add Cloudflare Pages build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+FLUTTER_VERSION="3.32.0"
+FLUTTER_DIR="$HOME/flutter"
+
+if [ ! -d "$FLUTTER_DIR" ]; then
+  curl -fsSL "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz" -o flutter.tar.xz
+  tar -xf flutter.tar.xz -C "$HOME"
+  rm flutter.tar.xz
+fi
+
+export PATH="$FLUTTER_DIR/bin:$PATH"
+
+flutter config --no-analytics
+flutter pub get
+flutter build web \
+  --dart-define=APP_KEY=${APP_KEY} \
+  --dart-define=APP_SECRET=${APP_SECRET}
